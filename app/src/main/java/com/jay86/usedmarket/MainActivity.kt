9e0a9435eaf0cac96af7fa8fda3f19e9a0afcc0b
Jay <Jay68@foxmail.com>
2017-10-10 17:30:25 +0800

    create project

---
+++ app/src/main/java/com/jay86/usedmarket/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jay86.usedmarket
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
